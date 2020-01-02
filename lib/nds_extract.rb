43ee9f5d923990158446ba6388d755e41e5cd93f
Olivia Madden <madden.livi@gmail.com>
2020-01-02 02:31:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -47,7 +47,9 @@ def total_gross(source)
   total_gross = 0 
   row_index = 0 
   while row_index < source.size do 
-    total_gross += gross_for_director(row_index)
+    
+    
+    total_gross += gross_for_director(row_index[0])
     row_index += 1 
   end
   total_gross
